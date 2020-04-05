f4fbcd8d19d31ed2759a63f92eba48824c9bb5c8
Aleksei Nikolaev <madd.niko@gmail.com>
2020-04-05 16:27:34 +0400

    style Комментарии

--- controllers/Shop/CartController.php
+++ controllers/Shop/CartController.php
@@ -1,15 +1,25 @@
 <?php
+/**
+ * Контроллер работы с корзиной товаров
+ *
+ * @package Shop
+ * @author Aleksei Nikolaev <madd.niko@gmail.com>
+ */
 
 namespace app\controllers\Shop;
 
 use yii\base\Exception;
 use yii\web\Controller;
-use app\models\Shop\Products;
 use app\models\Shop\Cart;
 use yii\web\Response;
 
 class CartController extends Controller {
 
+    /**
+     * Страница товаров в корзине
+     *
+     * @return mixed
+     */
     public function actionIndex() {
         $Cart = new Cart();
         $products = $Cart->getProducts();
@@ -20,12 +30,17 @@ class CartController extends Controller {
         }
     }
 
+    /**
+     * Добавление товара
+     *
+     * @return string
+     */
     public function actionAdd() {
         $Cart = new Cart();
         if (! \Yii::$app->request->isPost) {
             return $this->redirect(['cart/index']);
         }
-
+        /** @var array productId - ID товара, count - количество */
         $data = \Yii::$app->request->post();
         if (! isset($data['productId'])) {
             return $this->redirect(['cart/index']);
@@ -37,7 +52,12 @@ class CartController extends Controller {
         return $Cart->addToCart($data['productId'], $data['count']);
     }
 
-    public function actionRenderCart() {
+    /**
+     * Получение корзины для сокращенного отображения
+     *
+     * @return array
+     */
+    public function actionRenderCart(): array {
         $Cart = new Cart();
         $result = [
             'productsTotal' => $Cart->getCartTotal(),
@@ -50,15 +70,27 @@ class CartController extends Controller {
         return $result;
     }
 
+    /**
+     * Удаление товара
+     *
+     * @return string
+     */
     public function actionRemove() {
         $Cart = new Cart();
         $data = \Yii::$app->request->post();
+
         return $Cart->removeFromCart($data['productId']);
     }
 
+    /**
+     * Пересчет стоимости товаров
+     *
+     * @return bool
+     */
     public function actionRecountCart(){
         $Cart = new Cart();
         $data = \Yii::$app->request->get();
+
         return $Cart->recountCart($data);
     }
 
